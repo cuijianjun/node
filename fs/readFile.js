3ea12cf04537175573e3b190f6ad6aebf0ac6530
Frank <376247934@qq.com>
2021-04-13 10:38:20 +0800

    fs

--- fs/readFile.js
+++ fs/readFile.js
@@ -15,3 +15,18 @@ fs.readFile("cont.txt","UTF-8",function(err,data){
 		console.log(data);
 	}
 })
+
+// 异步读取
+
+fs.readFile("./cont.txt", (err, data) => {
+	if (err) {throw err};
+	console.log("data", data.toString());
+})
+
+(async () => {
+	const fs = require('fs');
+	const {promisify} = require('util');
+	const readFile = promisify(fs.readFile)
+	const data = await readFile('./conf.js')
+	console.log("data", data.toString());
+})()
--- http/app.js
+++ http/app.js
@@ -1,8 +1,42 @@
 var http = require('http');
+const fs = require('fs')
+const { fstat } = require('node:fs');
+const { request } = require('node:http');
+const { response } = require('express');
 
 http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.write('<h1>Node.js</h1>');
-	res.end('<p>PACT</p>');
+	// console.log("this is a req", getPrototypeChain(req));
+	// res.writeHead(200,{'Content-Type':'text/html'});
+	// res.write('<h1>Node.js</h1>');
+	// res.end('<p>PACT</p>');
+	const {url, method} = request;
+	if (url === "/" && method === "GET") {
+		fs.readFile('index.txt', (err, data) => {
+			if (err) {
+				req.writeHead(500, {"Content-Type": "text/plain;charset=utf-8"});
+				req.end("500 服务器错误 哈哈")
+				return
+			}
+			res.statusCode = 200
+			res.setHeader('Content-Type', "text/html")
+			res.end(data)
+		})
+	} else if (url === 'users' && method === 'GET') {
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(JSON.stringify({name: "TOM"}));
+	} else {
+		res.statusCode = 404
+		res.setHeader('Content-Type','text/html');
+		res.end('404 page')
+	}
 }).listen(3000);
 console.log('HTTP server is listening at port 3000')
+
+// 打印原型
+function getPrototypeChain(obj) {
+	const protoChain = []
+	while(obj = Object.getPrototypeOf(obj)) {
+		protoChain.push(obj)
+	}
+	return protoChain
+}
